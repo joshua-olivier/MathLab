c535ae6effa7f21263c4a561e6e15cb5d3c3c793
Joshua Olivier <108597396+joshua-olivier@users.noreply.github.com>
2022-10-22 20:24:46 -0400

    Add files via upload

---
+++ math.js
@@ -0,0 +1,49 @@
+window.alert("Keep in mind that the questions will get complex overtime. You will have to answer all 6 questions! 😃")
+
+prompt("Question #1: There are 1129 students at Maplewood Primary school. 679 of the students are girls. How many students are boys?");
+a = 1129
+b = 679
+c = a - b
+console.log(c)
+console.log("There are 450 boys at Maplewood Primary School.")
+
+prompt("Question #2: During the first week, 45 684 people went to the carnival. The second week's attendance was increased by 37 849. How many people went to the carnival during the second week?")
+
+c = 45684
+d = 37849
+e = c + d
+
+console.log(e)
+console.log("There are" + " " + e + " people who went to the carnival during the second week.")
+
+prompt("Question #3: On average, each plane takes 240 passengers and 12 tons of cargo. How many passengers depart from the airport every hour during the Christmas holidays? ")
+f = 240
+g = 12
+h = f*g
+
+console.log(h)
+console.log(h + " passengers depart from the airport every hour during the Christmas holidays")
+
+prompt("Question #4: During the summer holidays, your brother earns extra money mowing lawns. He mows six lawns an hour and has 21 lawns to mow. How long will it take him?")
+i = 21
+j = 6
+k = i/j
+
+console.log(k)
+console.log ("It will take him " + k + " hours to mow lawns.")
+
+prompt("Question #5: Your mom bought you a one-year swimming pass for $390. She is making 12 payments of how much money to pay for the pass?")
+l = 390
+m = 12
+n = l/m
+console.log(n)
+console.log ("She will have to pay " + n + " dollars.")
+
+prompt("Question #6: Mount Everest is 29, 028 feet high. Mount Kilimanjaro is 19,340 feet high. How much taller is Mount Everest?")
+o = 29028
+p = 19340
+q = o - p
+console.log(q)
+console.log("Mount everest is taller by " + q + " feets")
+
+console.log("YOU WILL NOW HAVE TO GRADE YOURSELF OUT OF SIX! 🔥🔥🔥")
